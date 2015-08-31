9d88eb1ee57719a53a2c1e3162a3fbae06922931
Dmitry Gordeev <netimen@gmail.com>
2015-08-31 12:46:25 +0300

    java poet + auto-service

---
+++ demo/src/main/java/com/bookmate/libs/demo/traits/Reader.java
@@ -0,0 +1,17 @@
+/**
+ * Copyright (c) 2015 Bookmate.
+ * All Rights Reserved.
+ * <p/>
+ * Author: Dmitry Gordeev <netimen@dreamindustries.co>
+ * Date:   28.08.15
+ */
+package com.bookmate.libs.demo.traits;
+
+import com.bookmate.traits.Event;
+
+public class Reader {
+    @Event
+    void f() {
+
+    }
+}
--- traits-apt/src/main/java/com/bookmate/traits/AnnotationProcessor.java
+++ traits-apt/src/main/java/com/bookmate/traits/AnnotationProcessor.java
@@ -1,29 +1,65 @@
 package com.bookmate.traits;
 
+import com.google.auto.service.AutoService;
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.TypeSpec;
+
+import java.io.IOException;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
-import javax.annotation.processing.ProcessingEnvironment;
+import javax.annotation.processing.Processor;
 import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.annotation.processing.SupportedSourceVersion;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
 import javax.tools.Diagnostic;
+import javax.tools.JavaFileObject;
 
 @SupportedAnnotationTypes("com.bookmate.traits.Event")
+@SupportedSourceVersion(SourceVersion.RELEASE_7)
+@AutoService(Processor.class)
 public class AnnotationProcessor extends AbstractProcessor {
 
-    @Override
-    public synchronized void init(ProcessingEnvironment processingEnv) {
-        super.init(processingEnv);
-
-        processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "AAAA");
-    }
+//    @Override
+//    public synchronized void init(ProcessingEnvironment processingEnv) {
+//        super.init(processingEnv);
+//    }
 
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-        processingEnv.getMessager().printMessage(Diagnostic.Kind.WARNING, "AAAA");
-        processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "AAAA");
-//        for (Element elem : roundEnv.getElementsAnnotatedWith(Event.class)) {
+        MethodSpec main = MethodSpec.methodBuilder("main")
+                .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
+                .returns(void.class)
+                .addParameter(String[].class, "args")
+                .addStatement("$T.out.println($S)", System.class, "Hello, JavaPoet!")
+                .build();
+
+        TypeSpec helloWorld = TypeSpec.classBuilder("HelloWorld")
+                .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
+                .addMethod(main)
+                .build();
+
+        JavaFile javaFile = JavaFile.builder("com.example.helloworld", helloWorld)
+                .build();
+
+//        if (annotations.size() > 0)
+//            for (TypeElement annotation : annotations)
+//                processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "AAAA" + annotation.getEnclosingElement().getSimpleName() + " " + annotation.getSimpleName());
+        for (Element elem : roundEnv.getElementsAnnotatedWith(Event.class)) {
+            final Element classElement = elem.getEnclosingElement();
+            processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, elem.getSimpleName() + " BBB " + classElement.getSimpleName());
+            try {
+                JavaFileObject jfo = null;
+                jfo = processingEnv.getFiler().createSourceFile(classElement.getSimpleName());
+                javaFile.writeTo(jfo.openWriter());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 //            Event complexity = elem.getAnnotation(Event.class);
 //            String message = "annotation found in " + elem.getSimpleName()
 //                    + " with complexity " ;
@@ -56,14 +92,8 @@ public class AnnotationProcessor extends AbstractProcessor {
 //
 //            }
 //        }
+        }
         return true; // no further processing of this annotation type
     }
 
-//    @Override
-//    public Set<String> getSupportedAnnotationTypes() { // CUR
-//        processingEnv.getMessager().printMessage(Diagnostic.Kind.WARNING, "ABAA");
-//        final Set<String> supportedAnnotationTypes = new HashSet<>(super.getSupportedAnnotationTypes());
-//        supportedAnnotationTypes.add("com.bookmate.traits.Event");
-//        return supportedAnnotationTypes;
-//    }
 }
