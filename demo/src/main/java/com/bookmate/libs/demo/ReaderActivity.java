b556cd7a1afed4aeae88bdf4914fdedc73f41bed
Dmitry Gordeev <netimen@gmail.com>
2015-10-05 14:31:39 +0300

    reader activity

---
+++ demo/src/main/java/com/bookmate/libs/demo/ReaderActivity.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) 2015 Bookmate.
+ * All Rights Reserved.
+ * <p/>
+ * Author: Dmitry Gordeev <netimen@dreamindustries.co>
+ * Date:   05.10.15
+ */
+package com.bookmate.libs.demo;
+
+import android.support.v7.app.AppCompatActivity;
+
+import com.bookmate.libs.demo.traits.bookmate.BookmateReader;
+import com.bookmate.libs.demo.traits.bookmate.BookmateReaderFragment_;
+import com.bookmate.libs.demo.traits.readercode.Document;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+
+@EActivity
+public class ReaderActivity extends AppCompatActivity {
+    @AfterViews
+    void ready() {
+        getFragmentManager().beginTransaction().add(BookmateReaderFragment_.builder().bookmateReader(new BookmateReader(new Document(), "bookmark", "discovered")).build(), "tag");
+    }
+}
--- demo/src/main/java/com/bookmate/libs/demo/traits/SelectionTrait.java
+++ demo/src/main/java/com/bookmate/libs/demo/traits/SelectionTrait.java
@@ -8,6 +8,7 @@
 package com.bookmate.libs.demo.traits;
 
 import com.bookmate.libs.demo.traits.readercode.SelectionStart;
+import com.bookmate.libs.traits.Event;
 
 import org.androidannotations.annotations.EBean;
 
@@ -15,6 +16,7 @@ import org.androidannotations.annotations.EBean;
 @EBean
 public class SelectionTrait extends ReaderTrait {
 
+    @Event
     void onSelectionStart(SelectionStart event) {
 
     }
